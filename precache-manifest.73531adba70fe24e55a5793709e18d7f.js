1bf5040f2ae592064dae92873d0c4b279b35705f
BZZANG <gess475@gmail.com>
2019-01-31 19:55:31 +0900

    Updates

---
+++ precache-manifest.73531adba70fe24e55a5793709e18d7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "5f4de99f6295a2edbae1",
+    "url": "/movie-app/static/js/main.5f4de99f.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie-app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "5f4de99f6295a2edbae1",
+    "url": "/movie-app/static/css/main.c7abf109.chunk.css"
+  },
+  {
+    "revision": "c65e86497dcc6b73d0f2e55f019ff136",
+    "url": "/movie-app/index.html"
+  }
+];
